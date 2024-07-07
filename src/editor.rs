8f1e5c6c94b6caa644072902e56c7cdd72dd8222
Subhra Sekhar Garai <Znackt@proton.me>
2024-07-07 23:31:03 +0530

    new fn repl added

--- src/editor.rs
+++ src/editor.rs
@@ -2,44 +2,31 @@ use crossterm::event::{read, Event::Key, KeyCode::Char};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use std::io::{self, Read};
 pub struct Editor {}
+
 impl Editor {
     pub fn default() -> Self {
         Editor {}
     }
-
     pub fn run(&self) {
-        enable_raw_mode().unwrap();
-        for b in io::stdin().bytes() {
-            match b {
-                Ok(b) => {
-                    let c = b as char;
-                    if c.is_control() {
-                        println!("Binary: {b:08b} ASCII: {b:#03} \r");
-                    } else {
-                        println!("Binary: {b:08b} ASCII: {b:#03} Character: {c:#?}\r");
-                    }
-                    if c == 'q' {
-                        break;
-                    }
-                }
-                Err(err) => println!("Error: {err}"),
-            }
+        if let Err(err) = self.repl() {
+            panic!("{err:#?}");
         }
-
-        loop {
-            match read() {
-                Ok(Key(event)) => {
+        print!("Goodbye.\r\n");
+    }
+    
+    pub fn repl(&self) -> Result<(), std::io::Error> {
+        enable_raw_mode()?;
+            loop {
+                if let Key(event) = read()? {
                     println!("{event:?} \r");
                     if let Char(c) = event.code {
-                        if c == 'q' {
+                        if c == 'q'{
                             break;
                         }
                     }
                 }
-                Err(err) => println!("Error: {err}"),
-                _ => (),
             }
-        }
-        disable_raw_mode().unwrap();
+        disable_raw_mode()?;
+        Ok(())
     }
 }
